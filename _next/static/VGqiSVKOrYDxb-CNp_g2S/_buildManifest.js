6235ed8d7a30b509422ca46441b2be940cd78b90
ATK3Shape <Alexandr.Tkachenko@3shape.com>
2023-03-09 15:22:12 +0000

    Deploying to gh-pages from @ ATK3Shape/nextjs-gtm-experiment@c516ec7c1387794f5aa6bc677d365ce278aa3b43 🚀

--- _next/static/VGqiSVKOrYDxb-CNp_g2S/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b006b331397fd480.css","static/chunks/pages/index-c0e30542e3bb6751.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/kuET3A-1wiItZ2SmycnfM/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b006b331397fd480.css","static/chunks/pages/index-eea8a46e7839f90a.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
