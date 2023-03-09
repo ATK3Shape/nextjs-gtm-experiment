c516ec7c1387794f5aa6bc677d365ce278aa3b43
ATK3Shape <Alexandr.Tkachenko@3shape.com>
2023-03-09 17:21:11 +0200

    Update welcome text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,10 +16,7 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
+          <p>Welcome to m GTM test website!</p>
           <div>
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
