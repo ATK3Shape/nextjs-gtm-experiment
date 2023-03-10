13285158c7685995c6afcf44adc8ea15ef055b46
ATK3Shape <Alexandr.Tkachenko@3shape.com>
2023-03-10 02:22:51 +0200

    Refactor gtm scripts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,24 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 // import GoogleAnalytics from '../components/GoogleAnalytics'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       {/* <GoogleAnalytics /> */}
+      <Script
+        id="google-analytics-script"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PWCRZVS');
+          `,
+        }}
+      />
       <Component {...pageProps} />
     </>
   )
